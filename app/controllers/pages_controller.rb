5da6058063afc7315b869608ed1fd65907afc30f
PingoPlay <rogriff@gmail.com>
2016-02-10 05:47:31 +0000

    create rails app and setup home, about, student, admin, blog pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+  
+  def home
+    
+  end
+  
+  def about
+    
+  end
+  
+  def admin
+    
+  end
+  
+  def student
+    
+  end
+  
+  def blog
+    
+  end
+  
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,23 @@
+class WelcomeController < ApplicationController
+  
+  def home
+    
+  end
+  
+  def about
+    
+  end
+  
+  def admin
+    
+  end
+  
+  def student
+    
+  end
+  
+  def blog
+    
+  end
+  
+end
